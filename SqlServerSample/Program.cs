8636059b9ef8d2e21f18a311522daa8ad2318167
Yusuke Sakamoto <sakamoto@technoface.co.jp>
2022-02-17 13:39:02 +0900

    CONVERT(VARCHAR(10), {1}, 112) をつかって、開始終了時間のスタイルをyyyymmddに変更

--- SqlServerSample/Program.cs
+++ SqlServerSample/Program.cs
@@ -21,9 +21,17 @@ namespace SqlServerSample
                 string dateCol = "date";
                 string startDate = "2022-02-17 00:00:00";
                 string endDate = "2022-02-19 00:00:00";
+                
+                string startDate2 = "20220217";
+                string endDate2 = "20220219";
 
                 string sqlTmpl = "SELECT * FROM {0} WHERE {1} BETWEEN '{2}' AND '{3}'";
-                string sql = string.Format(sqlTmpl, tableName, dateCol, startDate, endDate);
+                string sqlTmpl2 = "SELECT * FROM {0} WHERE CONVERT(VARCHAR(10), {1}, 112) BETWEEN '{2}' AND '{3}'";
+
+                //string sql = string.Format(sqlTmpl, tableName, dateCol, startDate, endDate);
+                string sql = string.Format(sqlTmpl2, tableName, dateCol, startDate2, endDate2);
+                
+                Console.WriteLine("SQL:" + sql);
 
                 // Connect to SQL
                 Console.Write("Connecting to SQL Server ... ");
