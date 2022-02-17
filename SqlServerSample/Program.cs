83f700c03168810ae59d3e27ae3d90ae97e8bab2
Yusuke Sakamoto <sakamoto@technoface.co.jp>
2022-02-17 13:28:33 +0900

    日付範囲指定

--- SqlServerSample/Program.cs
+++ SqlServerSample/Program.cs
@@ -16,6 +16,14 @@ namespace SqlServerSample
                 builder.InitialCatalog = "study1";
                 builder.IntegratedSecurity = true;
 
+                string tableName = "Sample1";
+                string idCol = "id";
+                string dateCol = "date";
+                string startDate = "2022-02-17 00:00:00";
+                string endDate = "2022-02-19 00:00:00";
+
+                string sqlTmpl = "SELECT * FROM {0} WHERE {1} BETWEEN '{2}' AND '{3}'";
+                string sql = string.Format(sqlTmpl, tableName, dateCol, startDate, endDate);
 
                 // Connect to SQL
                 Console.Write("Connecting to SQL Server ... ");
@@ -23,6 +31,19 @@ namespace SqlServerSample
                 {
                     connection.Open();
                     Console.WriteLine("Done.");
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader dataReader = command.ExecuteReader())
+                        {
+                            while (dataReader.Read())
+                            {
+                                string id = dataReader[idCol].ToString();
+                                string date = dataReader[dateCol].ToString();
+                                Console.WriteLine(id + ", " + date);
+                            }
+                        }
+                    }
                 }
             }
             catch (SqlException e)
